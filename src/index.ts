5061d6eaf34d9b5858a4dd3e41742e3779969ab9
Juan Manuel Barria <103613099+juanbarria@users.noreply.github.com>
2022-05-31 10:59:49 -0300

    corrección1

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-let arreglo : number = 6;
-let vector1 : number [] = [1,3,7,9,9,5];
-let vector2 : number [] = [6,9,2,5,9,4];
-let sumaVector : number [] = new Array (arreglo);
-let indice : number;
-for (indice=0 ; indice < arreglo; indice ++) {
-  sumaVector[indice] = vector1[indice] + vector2[indice] ; 
-};
+let arregloDimension: number = 6;
+let vector1: number[] = new Array(arregloDimension);
+let numero1: number;
+for (let indice = 0; indice < arregloDimension; indice++) {
+  numero1 = Number( prompt("Arreglo1: Indique el numero que desea incorporar en la posicion " + indice )  );
+  vector1[indice] = numero1;
+}
 
-console.log ("la suma es"  +sumaVector);
+let vector2: number[] = new Array(arregloDimension);
+let numero2: number;
+for (let indice = 0; indice < arregloDimension; indice++) {
+  numero2 = Number( prompt("Arreglo2: Indique el numero que desea incorporar en la posicion " + indice ) );
+  vector2[indice] = numero2;
+}
+
+let sumaVector: number[] = new Array(arregloDimension);
+for (let indice = 0; indice < arregloDimension; indice++) {
+  sumaVector[indice] = vector1[indice] + vector2[indice];
+}
+
+console.log("la suma es " + sumaVector);
